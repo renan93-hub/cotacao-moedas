07a3c27d8b37b362ff098dbf3903461f4f41294a
Renan Ferreira Renzo <66973821+renan93-hub@users.noreply.github.com>
2023-08-22 17:36:00 -0300

    Projeto Completo

---
+++ 3.Javascript - Trabalhando com Threads para requisições simultâneas/script/scripts.js
@@ -0,0 +1,84 @@
+import selecionaCotacao from "./imprimeCotacao.js";
+
+const graficoDolar = document.getElementById('graficoDolar');
+
+const graficoParaDolar = new Chart(graficoDolar, {
+    type: 'line',
+    data: {
+      labels: [],
+      datasets: [{
+        label: 'Dólar',
+        data: [],
+        borderWidth: 1
+      }]
+    },
+});
+
+function geraHorario(){
+    let data = new Date()
+    let horario = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds()
+    console.log(horario)
+    return horario
+}
+
+function adicionarDados(grafico, legenda, dados){
+    grafico.data.labels.push(legenda)
+    grafico.data.datasets.forEach((dataset) => {
+        dataset.data.push(dados)
+    })
+    grafico.update()
+}
+
+let workerDolar = new Worker('./script/workers/workersDolar.js')
+workerDolar.postMessage('usd')
+
+workerDolar.addEventListener("message", event => {
+    let tempo = geraHorario()
+    let valor = event.data.ask
+    selecionaCotacao("dolar", valor)
+    adicionarDados(graficoParaDolar, tempo, valor)
+})
+
+const graficoIene = document.getElementById('graficoIene')
+const graficoParaIene = new Chart(graficoIene, {
+    type: 'line',
+    data: {
+        labels:[],
+        datasets:[{
+            label: 'Iene',
+            data: [],
+            borderWidith: 1
+        }]
+    }
+})
+
+let workerIene = new Worker("./script/workers/workerIene.js")
+workerIene.postMessage("iene")
+workerIene.addEventListener("message", event => {
+    let tempo = geraHorario()
+    let valor = event.data.ask
+    adicionarDados(graficoParaIene, tempo, valor)
+    selecionaCotacao("iene", valor)
+})
+
+const graficoEthereum = document.getElementById('graficoEthereum')
+const graficoParaEthereum = new Chart(graficoEthereum, {
+    type: 'line',
+    data: {
+        labels:[],
+        datasets:[{
+            label: 'Ethereum',
+            data: [],
+            borderWidith: 1
+        }]
+    }
+})
+
+let workerEthereum = new Worker("./script/workers/workerEthereum.js")
+workerEthereum.postMessage("Ethereum")
+workerEthereum.addEventListener("message", event => {
+    let tempo = geraHorario()
+    let valor = event.data.ask
+    adicionarDados(graficoParaEthereum, tempo, valor)
+    selecionaCotacao("ethereum", valor)
+})
